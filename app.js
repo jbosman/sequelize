e4c0e58c7ee1cf29821c3da392078cde7e648d74
jeff-bruemmer <jeff.bruemmer@gmail.com>
2016-08-08 16:36:38 -0400

    Added models

--- app.js
+++ app.js
@@ -2,27 +2,24 @@ var chalk = require('chalk');
 var morgan = require('morgan');
 var path = require('path');
 var express = require('express');
-
+var models = require('./models');
 var app = express();
 var routes = require("./routes");
-
-
 var swig = require('swig');
 var bodyParser = require('body-parser');
 
+var page = models.Page.sync({force:true});
+var user = models.User.sync({force:true});
+
+
 // Swig settings
-//app.set('views', __dirname + '/views'); // point res.render to the proper directory
 app.set(path.join(__dirname, '/views'));
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', swig.renderFile); // when giving html files to res.render, tell it to use swig
 swig.setDefaults({ cache: false });		// Turns off caching
 
-app.use(express.static('./stylesheets/style.css'));
 
-// Middleware functions
-// function ServerMessage(string){
-// 	console.log(chalk.blue(string));
-// }
+app.use(express.static('./stylesheets/style.css'));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
---
+++ models/index.js
@@ -0,0 +1,55 @@
+var Sequelize = require('sequelize');
+var db = new Sequelize('postgres://localhost:5432/wikistack', {
+    logging: false
+});
+
+
+var Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING,
+	allowNull: false,
+	defaultValue: "Title"
+    },
+    urlTitle: {
+        type: Sequelize.STRING,
+	allowNull: false
+    },
+    content: {
+        type: Sequelize.TEXT,
+	allowNull: false,
+	defaultValue: "Content"
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    },
+},
+		     {
+			 getterMethods: {
+				page: function() {
+				    return {
+					title: this.title,
+					url: '/wiki/' + this.urlTitle,
+					content: this.content,
+					status: this.status
+				    };
+				}
+			    }
+
+});
+
+var User = db.define('user', {
+    name: {
+        type: Sequelize.STRING,
+	allowNull: false,
+	defaultValue: "User"
+    },
+    email: {
+        type: Sequelize.STRING,
+	allowNull: false
+    }
+});
+
+module.exports = {
+  Page: Page,
+  User: User
+};
