532ed617499aaa020acd9096fd4f9e63ab08cf00
jeff-bruemmer <jeff.bruemmer@gmail.com>
2016-08-08 18:41:21 -0400

    need to add page

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,44 +1,55 @@
 var express = require('express');
 var wikiRouter = express.Router();
-
 var models = require('../models');
 var Page = models.Page; 
 var User = models.User; 
+wikiRouter.get('/wiki/add', function(req, res, next) {
+    res.render('addpage');
+});
 
-wikiRouter.post('/', function(req, res, next) {
+wikiRouter.post('/wiki/add', function(req, res, next) {
 
   // STUDENT ASSIGNMENT:
-  // add definitions for `title` and `content`
-
+    // add definitions for `title` and `content`
+    var url = req.body.title.split(' ').join('_');
+    url = url; 
+    
   var page = Page.build({
-    title: req.body.title,
-    content: req.body.content
+      title: req.body.title,
+      content: req.body.content,
+      urlTitle: url
   });
 
   // STUDENT ASSIGNMENT:
   // make sure we only redirect *after* our save is complete!
-  // note: `.save` returns a promise or it can take a callback.
-  page.save();
-  // -> after save -> res.redirect('/');
+    // note: `.save` returns a promise or it can take a callback
+
+    page.save()
+	.then(function() {
+	res.redirect(url);
+	})
+	.catch(function(err) {
+	throw new Error(err);
+    });
 });
 
+wikiRouter.get('/wiki/:page', function(req, res, next) {
 
-
-wikiRouter.get('/wiki/add/', function(req, res){
-	res.render("addpage");
+    console.log("url:", req.params.page);
+    Page.findOne({
+	where: {
+	    urlTitle: req.params.page 
+	}
+    })
+	.then(function(foundPage){
+	    res.json(foundPage);
+	})
+	.catch(next);
+    
 });
-
-wikiRouter.post('/wiki', function(req, res){
-	res.send(req.body);
-	res.redirect('/wiki/');
-});
-
-
 
 wikiRouter.get('/wiki/', function(req, res){
-	//res.send("Made it here");
 	res.render("wikipage");
-	//res.redirect('/');
 });
 
 
